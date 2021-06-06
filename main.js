07ec19a9cb691e0c0ac14aeec9e74d694f0b175b
Andrew McIndoe <amcindoe@powerhrg.com>
2021-06-06 17:12:26 -0400

    Simple-Liker Lab

--- main.js
+++ main.js
@@ -2,10 +2,76 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//---------------------------------------------------------------------------
+
+//will need to add .hidden class to "id = Model in error modal"
+// const likeClick = document.querySelector('.like-glyph')
+
+
+// document.addEventListener("click", changeHeart)
+
+// function changeHeart (){
+//   likeClick.addEventListener('click', function(){
+//   mimicServerCall() 
+//   .then()
+/*Need to be able to call upon activated hear class and attach to target
+element. 
+Need to switch out const FULL_HEART & EMPTY HEART to switch fotter display
+*/
+const commentHearts = document.querySelectorAll(".like-glyph");
+
+document.addEventListener("click", changeHeart)
+function changeHeart(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+  .catch(function(error){;
+    console.log(error.message);
+    const objError = document.querySelector('#modal')
+    const oErrorMessage = document.querySelector('#modal-message')
+    objError.classList.remove('hidden')
+    oErrorMessage.innerText = error
+    setTimeout(() =>{
+      objError.classList.add('hidden')
+    }, 3000)
+  })
+
+      
+
+        /* fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); */
+
+        
+
+  
 
 
 
+
+
+
+  //Need to make function that simulates user request to like a comment
+//will need to change heart from empty to full or full to empty
+//when clicked or changed, will need and event lisenter. 
+//if request fails then will need to remove hidden status for error modal
+// for three seconds(will need set Timeout method)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -22,4 +88,9 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       }
     }, 300);
   });
+}
+}
+
+for (const glyph of commentHearts) {
+  glyph.addEventListener("click", likeCallback);
 }
